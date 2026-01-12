e5ba5a4e4a9b77b116bdc4af4d909d63e264b3a0
Vladislav Tetcoshvili <v.tetcoshvili@petrovich.tech>
2026-01-12 05:25:12 +0300

    Clean

--- src/main/java/com/javarush/caesar/exception/CaesarRunTimeException.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.caesar.exception;
-// Непроверяемые исключения
-public class CaesarRunTimeException extends RuntimeException {
-    public CaesarRunTimeException(String message) {
-        super(message);
-    }
-}
